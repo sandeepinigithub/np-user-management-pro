74468eaa2463c9d5958434950310c5649836d569
Sandeep Shukla <sandeepini.2012@gmail.com>
2021-07-08 09:05:46 +0530

    done from my side little bug is there

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,19 @@
 import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements DoCheck {
+export class HeaderComponent implements DoCheck , OnChanges{
 
   href:string = "";
   loginPageActive:boolean=true;
   hideSigninSignup:boolean=true;
-  constructor(private router: Router) { }
+  isLoggedin : boolean = false;
+  constructor(private router: Router ,private authService : AuthService) { }
 
   ngDoCheck(): void {
     this.href = this.router.url;
@@ -23,8 +25,16 @@ export class HeaderComponent implements DoCheck {
     }
     else{
       this.loginPageActive = true;
-    }   
-
+    }  
+    
+    this.isLoggedin = this.authService.getLogin();
+    
+  }
+  ngOnChanges(){
+    this.isLoggedin = this.authService.getLogin();
   }
 
+  logout(){
+    this.authService.setLogout();
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  activeUser: any[] = [];
+  isLoggedin: boolean = false;
+
+  constructor() {
+    if (localStorage.getItem('activeUser') != null) {
+      this.activeUser = JSON.parse(localStorage.getItem('activeUser') || '');
+    }
+  }
+
+  getLogin(): boolean {
+    if (this.activeUser.length != 0) {
+      this.isLoggedin = true;
+      return this.isLoggedin;
+    }
+    else {
+      this.isLoggedin = false;
+      return this.isLoggedin;
+    }
+  }
+  setLogout(): void {
+    localStorage.removeItem('activeUser');
+    this.isLoggedin = false;
+  }
+}
